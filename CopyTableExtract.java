87f320e2dbbab52ea53e799235943c6e6b7cfdd5
Thanushbj7 <111106003+Thanushbj7@users.noreply.github.com>
2024-01-23 14:39:56 +0530

    Update CopyTableExtract.java

--- CopyTableExtract.java
+++ CopyTableExtract.java
@@ -1,97 +1,158 @@
-<!--How to display all cases related details using lightning component
-Trying to display a list of records in the related list for the Case using lightning component.
-I am getting the Case Id and displaying all the casesaction related to that object.
--->
-<aura:component implements="flexipage:availableForAllPageTypes" access="global" controller = "CaseRelatedListApex" >
+Public with sharing class CaseRelatedListApex{
    
-
-   <!--   <aura:attribute name="pageTitle" type="String" default="CASE HISTORY"/>-->
-   <!--     <aura:attribute name="sessId" type="string"/>-->
-    <aura:attribute name="cases" type="Case[]"/>
-    <aura:handler name="init" value="{!this}" action="{!c.check}" />
-   <!-- <aura:attribute name="Cases" type="list"/>-->
-    <aura:attribute name="recordId" type="String" />
-    <aura:attribute name="Casenumber" type="String" />
-    <!--   <aura:handler event="c:CaseRelatedListEvent" action="{!c.handleCSDetailsEvent}" />-->
-     <lightning:card title="CASE HISTORY" iconName="standard:add_contact">
-    <div class="slds-col slds-col--padded slds-p-top--large">
- <div style="float: left; width: 20%; padding: 10px; ">
-        <h1>{!v.pageTitle}</h1>
-    </div>
-   <div style="float: left; width: 80%; padding: 10px;">
-   <table style="width: 100%; border-collapse: collapse;">
-        <thead>
-            <tr>
-                <th style="border: 1px solid #ddd; padding: 8px;">Case Number</th>
-                <th style="border: 1px solid #ddd; padding: 8px;">Date Time</th>
-                <th style="border: 1px solid #ddd; padding: 8px;">Plan ID</th>
-                <th style="border: 1px solid #ddd; padding: 8px;">Inquiry</th>
-                <th style="border: 1px solid #ddd; padding: 8px;">Transactions</th>
-                <th style="border: 1px solid #ddd; padding: 8px;">Account Maintenance</th>
-                <th style="border: 1px solid #ddd; padding: 8px;">Forms</th>
-                <th style="border: 1px solid #ddd; padding: 8px;">Other</th>
-               
-            </tr>
-        </thead>
-        <tbody>
-            <aura:iteration items="{!v.cases}" var="caseRecord">
-                
-				 <!--<aura:if isTrue="{!caseRecord.Case__r.CaseNumber==Casenumber}">-->
-                <tr>
-                    <td style="border: 1px solid #ddd; padding: 8px;">{!caseRecord.Case__r.CaseNumber}</td>
-                    <td style="border: 1px solid #ddd; padding: 8px;">{!caseRecord.Case__r.CreatedDate}</td>
-                    <td style="border: 1px solid #ddd; padding: 8px;">{!caseRecord.PlanID_Text__c}</td>
-                    <td style="border: 1px solid #ddd; padding: 8px;">
-                        <aura:if isTrue="{!caseRecord.Call_Activity__c=='Inquiry'}">
-                            {!caseRecord.Call_Type__c}
-                        </aura:if>
-                        
-                    </td>
-                    <td style="border: 1px solid #ddd; padding: 8px;">
-                        <aura:if isTrue="{!caseRecord.Call_Activity__c=='Transactions'}">
-                            {!caseRecord.Call_Type__c}
-                        </aura:if>
-                          
-                    </td>
-                     <td style="border: 1px solid #ddd; padding: 8px;">
-                        <aura:if isTrue="{!caseRecord.Call_Activity__c=='Account Maintenance'}">
-                            {!caseRecord.Call_Type__c}
-                        </aura:if>
-                </td>
-                    <td style="border: 1px solid #ddd; padding: 8px;">
-                        <aura:if isTrue="{!caseRecord.Call_Activity__c=='Forms'}">
-                            {!caseRecord.Call_Type__c}
-                        </aura:if>
-                </td>
-                    <!--assign case number-->
-                </tr>
-            </aura:iteration>
-        </tbody>
-    </table>
+    @AuraEnabled(cacheable=true)
+    public static List<Case_Actions__c> getObject(){  
+        LIST< Case_Actions__c > objCaseList = new LIST< Case_Actions__c >(); 
+        
+        objCaseList =[SELECT  Case__r.CaseNumber,Case__r.CreatedDate,PlanID_Text__c,Call_Activity__c,Call_Type__c FROM Case_Actions__c where  Case__r.Account.SSN__c IN  ('010820241')];
+        
+        return  objCaseList;
+        
+          }
+    public static List<Case_Actions__c> getcaseactionrecords(){
+        
+        List<Case_Actions__c> Caseaction = new List<Case_Actions__c>();
+       // if((String.isBlank(Caseid) || Caseid==''||Caseid==null) &&(String.isBlank(clientStatus) || clientStatus==''||clientStatus==null)){
+            Caseaction = [SELECT  Case__r.CaseNumber,Case__r.CreatedDate,PlanID_Text__c,Call_Activity__c,Call_Type__c 
+                               from Case_Actions__c where  Case__r.Account.SSN__c IN  ('010820241') ];
        
-        </div>
-        </div>
-       
-           </lightning:card>
-    </aura:component>
+       // }
+        
+        return generateWrapperData(Caseaction);
+    }
+    
+    private static List<Case_Actions__c> generateWrapperData(List<Case_Actions__c> Caseactionlist) {
+        List<Case_Actions__c> caseactionWrapperList = new List<Case_Actions__c>();
+        
+        for (Case_Actions__c Caction : Caseactionlist) {
+            
+            Case_Actions__c CactionWrap = new Case_Actions__c();
+            
+            CactionWrap.id = Caction.Id;
+            CactionWrap.PlanID_Text__c = Caction.PlanID_Text__c;
+            CactionWrap.Call_Activity__c = Caction.Call_Activity__c;
+            CactionWrap.Call_Type__c = Caction.Call_Type__c;          
+         
+            caseactionWrapperList.add(CactionWrap); 
+        }
+        
+        return caseactionWrapperList;
+    }
+    
+}
 
 
 
-	// caseHistoryLWC.js
-import { LightningElement, wire, api } from 'lwc';
-import getCaseRelatedList from '@salesforce/apex/CaseRelatedListApex.getObject';
+
+
+
+
+
+
+
+
+
+
+
+import { LightningElement,api,track,wire } from 'lwc';
+import { publish, MessageContext } from 'lightning/messageService';
+import EXAMPLE_MESSAGE_CHANNEL from '@salesforce/messageChannel/ExampleMessageChannel__c';
+import getObject from '@salesforce/apex/CaseRelatedListApex.getObject';
+//import getcaseactionrecords from '@salesforce/apex/CaseRelatedListApex.getcaseactionrecords';
+
+const columns = [
+    { label: 'Case Number', fieldName: 'CaseNumber' },
+    { label: 'Date', fieldName: 'CreatedDate' },
+    { label: 'Plan Id', fieldName: 'PlanID_Text__c' },
+    { label: 'Inquiry', fieldName: 'Call_Type__c' },
+    { label: 'Transactions', fieldName: 'Call_Type__c' },
+		 { label: 'Account Maintenance', fieldName: 'Call_Type__c' },
+		 { label: 'Forms', fieldName: 'Call_Type__c'},
+		{ label: 'Others', fieldName: 'Call_Type__c'},
+];
 
 export default class CaseHistoryLWC extends LightningElement {
-    @api recordId;
+		@track passedValue;
+		dnisNumber;
+		source;
+		CTIP
+		clientSSN;
+		ctiVRUApp;
+		ctiEDU;
+		AuthenticatedFlag;
+		caseOrigin;
 
-    cases;
+		@wire(MessageContext)
+		messageContext;
+		
+		@track data = [];
+    @track columns = columns;
+    wiredRecords;
+		@wire(getObject) wiredCases(value){
+			this.wiredRecords=value;
+				const {data, error}= value;
+				if(data){
+						
+					let tempRecords= JSON.parse(JSON.stringify(data));
+						
+							 tempRecords= tempRecords.map(row=>{
+									
+												 return {...row ,CaseNumber:row.Case__r.CaseNumber , CreatedDate:row.Case__r.CreatedDate};
+									 
+							 });
+						this.data= tempRecords;
+							console.log("tempRecords!" , tempRecords);
+				}
+				if(error){
+						console.log("error Occurred!" , error);
+				}
+		}
+    
 
-    @wire(getCaseRelatedList, { caseId: '$recordId' })
-    wiredCases({ error, data }) {
-        if (data) {
-            this.cases = data;
-        } else if (error) {
-            // Handle error
-        }
-    }
+		connectedCallback() {
+				// Parse the URL and get the 'passedValue' parameter
+				const urlParams = new URLSearchParams(window.location.search);
+				this.passedValue = urlParams.get('passedValue') || 'No value passed';
+				this.Id = urlParams.get('Id');		
+				this.source = urlParams.get('source');
+				this.clientSSN = urlParams.get('clientSSN');			
+				this.AuthenticatedFlag = urlParams.get('AuthenticatedFlag');
+				this.caseOrigin = urlParams.get('caseOrigin');
+				
+				console.log('Line 31 '+'Id '+this.Id+this.source+ this.clientSSN +this.AuthenticatedFlag+this.caseOrigin );
+				/*getPlans({ Id:this.Id, clientSSN:this.clientSSN, caseOrigin:this.caseOrigin})
+						.then(result => {
+						// Handle the returned list in the result
+						console.log('Returned List:', result);
+				})
+						.catch(error => {
+						// Handle any errors here
+						console.error('Error:', error);
+				});*/
+ 
+
+				try{
+						const MSG = {
+								message: 'Your message here'
+						};
+						// publish(this.messageContext, EXAMPLE_MESSAGE_CHANNEL, xyz);
+				}
+				catch(error){
+						console.error('Error publishing message', error);
+				}
+
+		}
+
+		sendMessage() {
+				try {
+						const message = {
+								message: 'Your message here'
+						};
+						publish(this.messageContext, EXAMPLE_MESSAGE_CHANNEL, message);
+				} catch (error) {
+						console.error('Error publishing message', error);
+				}
+		}
+
+
 }
